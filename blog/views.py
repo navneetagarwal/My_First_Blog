79fd9052e3646ea5890d6c232fcde61f0cccf342
Navneet Agarwal <navneetagarwal4@gmail.com>
2015-10-25 13:35:53 +0530

    Login

--- blog/views.py
+++ blog/views.py
@@ -3,15 +3,21 @@ from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 from .forms import PostForm
 # Create your views here.
+
+@login_required
 def post_list(request):
    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
    return render(request, 'blog/post_list.html', {'posts': posts})
 
+@login_required
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -24,6 +30,8 @@ def post_new(request):
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
+
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
